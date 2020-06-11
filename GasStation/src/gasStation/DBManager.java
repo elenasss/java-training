7800214452359f10aee60ecbe296d1c99c864c3f
Elena Sarbinska <sarbinska.elena@gmail.com>
2020-06-11 15:05:36 +0300

    GasStation

--- GasStation/src/gasStation/DBManager.java
+++ GasStation/src/gasStation/DBManager.java
@@ -8,7 +8,7 @@ public class DBManager {
     private static Connection connection;
     private static final String DB_IP = "127.0.0.1";
     private static final String DB_PORT = "3306";
-    private static final String DB_NAME = "GasStation";
+    private static final String DB_NAME = "gasStation.GasStation";
     private static final String DB_USER = "root";
     private static final String DB_PASS = "MyPass";
 
