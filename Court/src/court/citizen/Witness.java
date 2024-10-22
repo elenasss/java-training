5fbaeec9ca3b76307b8602abeb60a87419b09df0
elenasss <35662137+elenasss@users.noreply.github.com>
2024-10-22 18:29:42 +0300

    Delete Court directory

--- Court/src/court/citizen/Witness.java
+++
@@ -1,8 +0,0 @@
-package court.citizen;
-
-public class Witness extends Citizen {
-
-    public Witness(String fullName, String address, int age) {
-        super(fullName, address, age);
-    }
-}
--- Court/src/court/jurists/Jury.java
+++
@@ -1,9 +0,0 @@
-package court.jurists;
-
-public class Jury extends LegalPerson {
-
-    public Jury(String name, int experience, int numberOfCases) {
-        super(name, Occupation.JURY, experience, numberOfCases);
-    }
-
-}
